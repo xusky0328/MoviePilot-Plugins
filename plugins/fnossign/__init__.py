fbe421e965afc7b22ba22e34c12c0ee33c1f0cf3
madrays <keke210302@gmail.com>
2025-03-06 17:45:36 +0800

    修复飞牛论坛签到插件的语法错误

--- plugins/fnossign/__init__.py
+++ plugins/fnossign/__init__.py
@@ -44,7 +44,7 @@ class FnossignSigner:
     # 插件图标
     plugin_icon = "sign.png"
     # 插件版本
-    plugin_version = "1.0"
+    plugin_version = "1.3"
     # 插件作者
     plugin_author = "thsrite"
     # 作者主页
