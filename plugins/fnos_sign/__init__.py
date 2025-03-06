e0acc2bce37fd4a4421d55bcbd958e2436859746
madrays <keke210302@gmail.com>
2025-03-06 15:21:41 +0800

    fix: 更新版本号到1.3并添加更新说明

--- plugins/fnos_sign/__init__.py
+++ plugins/fnos_sign/__init__.py
@@ -23,7 +23,7 @@ class FnosSign(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/madrays/MoviePilot-Plugins/main/icons/fnos.ico"
     # 插件版本
-    plugin_version = "1.2"
+    plugin_version = "1.3"
     # 插件作者
     plugin_author = "madrays"
     # 作者主页
