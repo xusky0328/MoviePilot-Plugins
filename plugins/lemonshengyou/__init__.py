58da4dafce4f540c35caf9c94d4565ba8bfa6187
madrays <keke210302@gmail.com>
2025-03-13 00:24:47 +0800

    修复飞牛签到、柠檬神游、更新readme

--- plugins/lemonshengyou/__init__.py
+++ plugins/lemonshengyou/__init__.py
@@ -29,7 +29,7 @@ class lemonshengyou(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/madrays/MoviePilot-Plugins/main/icons/lemon.ico"
     # 插件版本
-    plugin_version = "1.0.4"
+    plugin_version = "1.0.5"
     # 插件作者
     plugin_author = "madrays"
     # 作者主页
